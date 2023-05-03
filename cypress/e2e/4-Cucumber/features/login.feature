398b5c9e8002bfc0283ece7f3e7bdb0e03615251
ravi.gajul <ravi.gajul@yahoo.com>
2023-05-03 23:54:39 +0530

    added additional scenario

--- cypress/e2e/4-Cucumber/features/login.feature
+++ cypress/e2e/4-Cucumber/features/login.feature
@@ -6,3 +6,10 @@ When I enter username webdriver
 And I enter password webdriver123
 And I click on login button
 Then I should be logged in
+
+Scenario: This is a another simple login scenario
+Given I navigate to the application
+When I enter username webdriver and password webdriver123
+And I click on login button
+Then I should be logged in
+And The alert is shown with message validation succeeded
--- cypress/support/step_definitions/login.js
+++ cypress/support/step_definitions/login.js
@@ -1,5 +1,10 @@
 import {Before,Given,When,Then,And} from "cypress-cucumber-preprocessor/steps";
+let stub;
+Before(()=>{
+    cy.log('Executing before step')
+    stub = cy.stub()
 
+})
 Given("I navigate to the application",()=>{
     cy.visit("http://www.webdriveruniversity.com/Login-Portal/index.html")
 });
@@ -9,14 +14,26 @@ When('I enter username {word}',(username)=>{
     
 });
 
+When('I enter username {word} and password {word}',(username,password)=>{
+    cy.get('#text').type(username)
+    cy.get('#password').type(password)
+});
+
 And('I enter password {word}',(password)=>{
     cy.get('#password').type(password)
 });
 
 And('I click on login button',()=>{
     cy.get('#login-button').click()
+    cy.on('window:alert',stub)
 });
 
 Then('I should be logged in',()=>{
     cy.log("i'm logged in")
 }); 
+
+And('The alert is shown with message {word} {word}',(message1,message2)=>{
+    const expectedMessage = message1+" " + message2;
+    cy.log(stub.getCall(0).lastArg);
+    expect(stub.getCall(0).lastArg).to.equal(expectedMessage);
+});
